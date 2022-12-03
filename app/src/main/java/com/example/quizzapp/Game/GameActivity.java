5b3a974997b5c19b1f2dc3b78b546bbd1049844a
clementseiler <clement70200@gmail.com>
2022-12-03 11:48:46 +0100

    Page d'acceuil V1 OK
    Start VMC

---
+++ app/src/main/java/com/example/quizzapp/Game/GameActivity.java
@@ -0,0 +1,31 @@
+package com.example.quizzapp.Game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.quizzapp.R;
+
+public class GameActivity extends AppCompatActivity {
+
+    private TextView mQuestionView;
+    private Button mAnswerOne;
+    private Button mAnswerTwo;
+    private Button mAnswerThree;
+    private Button mAnswerFour;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        mQuestionView = findViewById(R.id.game_activity_textview_question);
+        mAnswerOne = findViewById(R.id.game_activity_button_1);
+        mAnswerTwo = findViewById(R.id.game_activity_button_2);
+        mAnswerThree = findViewById(R.id.game_activity_button_3);
+        mAnswerFour = findViewById(R.id.game_activity_button_4);
+
+    }
+}
---
+++ app/src/main/java/com/example/quizzapp/Model/User.java
@@ -0,0 +1,13 @@
+package com.example.quizzapp.Model;
+
+public class User {
+    private String mFristName;
+
+    public String getmFristName() {
+        return mFristName;
+    }
+
+    public void setmFristName(String mFristName) {
+        this.mFristName = mFristName;
+    }
+}

078558f5eff0c312ce5a604247ad0a8026d6cf9d
clement <clement70200@gmail.com>
2022-12-16 00:08:14 +0100

    score et passage data de intent à intent

--- app/src/main/java/com/example/quizzapp/Game/GameActivity.java
+++ app/src/main/java/com/example/quizzapp/Game/GameActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,6 +38,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     private int NombresDeQuestions;
     private int mScores;
+
+    public static final String BUNDLE_EXTRA_SCORE = "BUNDLE_EXTRA_SCORE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -146,6 +149,9 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent();
+                            intent.putExtra(BUNDLE_EXTRA_SCORE,mScores);
+                            setResult(RESULT_OK,intent);
                             finish();
                         }
                     })
--- app/src/main/java/com/example/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.quizzapp;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -19,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
     private EditText mNameEditText;
     private Button mPlayButton;
     private User mUser;
+    private static final int GAME_ACTIVITY_REQUEST_CODE = 42;
+    private static final String SHARED_PREF_USER_INFO = "SHARED_PREF_USER_INFO";
+    private static final String SHARED_PREF_USER_INFO_NAME = "SHARED_PREF_USER_INFO_NAME";
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (GAME_ACTIVITY_REQUEST_CODE == requestCode){
+            int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE,0);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         mUser = new User();
 
         mPlayButton.setEnabled(false);
+
+        String mFirstName = getSharedPreferences(SHARED_PREF_USER_INFO,MODE_PRIVATE).getString(SHARED_PREF_USER_INFO_NAME,null);
 
         mNameEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -53,8 +69,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mUser.setmFristName(mNameEditText.getText().toString());
+
+                getSharedPreferences(SHARED_PREF_USER_INFO,MODE_PRIVATE)
+                        .edit()
+                        .putString(SHARED_PREF_USER_INFO_NAME, mUser.getmFristName())
+                        .apply();
+
                 Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(gameActivityIntent);
+                startActivityForResult(gameActivityIntent,GAME_ACTIVITY_REQUEST_CODE);
+
             }
         });
     }
